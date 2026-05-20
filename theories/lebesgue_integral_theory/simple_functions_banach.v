ba9dd32273ffe014f5e268e6d7c786510333de38
adjevahi <arthur.djevahirdjian@ens-lyon.fr>
2026-05-20 14:50:07 +0200

    put measurableTop in module HBTopMeas, started work on simple_functions_banach.v

--- theories/lebesgue_integral_theory/simple_functions_banach.v
+++ theories/lebesgue_integral_theory/simple_functions_banach.v
@@ -1,47 +1,22 @@
 From HB Require Import structures.
-From mathcomp Require Import all_ssreflect_compat all_algebra finmap.
-#[warning="-warn-library-file-internal-analysis"]
-From mathcomp Require Import unstable.
-From mathcomp Require Import mathcomp_extra boolp classical_sets functions cardinality reals.
-From mathcomp Require Import fsbigop tvs ereal topology normedtype sequences.
-From mathcomp Require Import real_interval esum measure lebesgue_measure numfun realfun measurable_realfun.
-From mathcomp Require Import measurable_structure.
+From mathcomp Require Import all_ssreflect_compat ssralg ssrnum ssrint interval.
+From mathcomp Require Import interval_inference archimedean finmap.
+From mathcomp Require Import mathcomp_extra boolp classical_sets functions.
+From mathcomp Require Import cardinality reals fsbigop ereal topology tvs.
+From mathcomp Require Import normedtype sequences real_interval esum measure.
+From mathcomp Require Import lebesgue_measure numfun realfun measurable_realfun.
+From mathcomp Require Import normed_module measurable_structure.
 
 Unset SsrOldRewriteGoalsOrder.  (* remove the line when requiring MathComp >= 2.6 *)
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
-Import ProperNotations.
 Import Order.TTheory GRing.Theory Num.Def Num.Theory.
 Import numFieldNormedType.Exports.
 
 Local Open Scope classical_set_scope.
 Local Open Scope ring_scope.
-
-
-Section Topological_sigmaring.
-Variable T : topologicalType.
-Let G := @open T.
-
-(* Doesn't work, even though when put in measurable_structure.v it works*)
-Definition measurableTop : set (set T) := G.-sigma.-measurable.
-
-Let measurable0T : measurableTop set0. Proof. exact: sigma_algebra0. Qed.
-Let measurableCT : forall A, measurableTop A -> measurableTop (~` A). 
-Proof. exact: sigma_algebraC. Qed.
-Let measurable_bigcupT : forall F : (set T)^nat, (forall i, measurableTop (F i)) -> 
-measurableTop (\bigcup_i (F i)). Proof. exact: sigma_algebra_bigcup. Qed.
-
-Definition measurableTypeTop := g_sigma_algebraType G.
-
-HB.instance Definition Top_isMeasurable :
-  isMeasurable default_measure_display T :=
-  @isMeasurable.Build _ measurableTypeTop measurableTop
-    measurable0T measurableCT measurable_bigcupT.
-
-End Topological_sigmaring.
-
-
+Local Open Scope measure_display_scope.
 
 Reserved Notation "{ 'nnfun' aT >-> T }"
   (at level 0, format "{ 'nnfun'  aT  >->  T }").
@@ -56,11 +31,18 @@ Reserved Notation "{ 'sfun' aT >-> T }"
 Reserved Notation "[ 'sfun' 'of' f ]"
   (at level 0, format "[ 'sfun'  'of'  f ]").
 
+Section Cast_test.
+Variables (rT : realType) (nT : normedModType rT).
+Check @open nT.
+Check measurableTop [set : nT].
+End Cast_test.
   
 Module HBSimple.
+Import HBTopMeas.
 
-HB.structure Definition SimpleFun d (aT : sigmaRingType d) (rT : realType) (nT : normedModType rT) :=
-  {f of @isMeasurableFun d _ aT nT f & @FiniteImage aT nT f}.
+HB.structure Definition SimpleFun d (aT : sigmaRingType d) 
+(T : topologicalType) :=
+  {f of @isMeasurableFun d _ aT T f & @FiniteImage aT T f}.
 
 End HBSimple.
 
@@ -80,8 +62,9 @@ Notation "{ 'nnsfun' aT >-> T }" := (@HBNNSimple.NonNegSimpleFun.type _ aT%type 
 Notation "[ 'nnsfun' 'of' f ]" := [the {nnsfun _ >-> _} of f] : form_scope.
 (* TODO : adapt this for Banach spaces as well*)
 Section sfun_pred.
-Context {d} {aT : sigmaRingType d} {rT : realType}.
-Definition sfun : {pred _ -> _} := [predI @mfun _ _ aT rT & fimfun].
+Context {d} {aT : sigmaRingType d} {T: topologicalType}.
+Import HBTopMeas.
+Definition sfun : {pred _ -> _} := [predI @mfun _ _ aT T & fimfun].
 Definition sfun_key : pred_key sfun. Proof. exact. Qed.
 Canonical sfun_keyed := KeyedPred sfun_key.
 Lemma sub_sfun_mfun : {subset sfun <= mfun}. Proof. by move=> x /andP[]. Qed.
@@ -89,16 +72,17 @@ Lemma sub_sfun_fimfun : {subset sfun <= fimfun}. Proof. by move=> x /andP[]. Qed
 End sfun_pred.
 
 Section sfun.
-Context {d} {aT : measurableType d} {rT : realType}.
-Notation T := {sfun aT >-> rT}.
-Notation sfun := (@sfun _ aT rT).
+Context {d} {aT : measurableType d} {T : topologicalType}.
+Import HBTopMeas.
+Notation Sf := {sfun aT >-> T}.
+Notation sfun := (@sfun _ aT T).
 Section Sub.
-Context (f : aT -> rT) (fP : f \in sfun).
+Context (f : aT -> T) (fP : f \in sfun).
 Definition sfun_Sub1_subproof :=
-  @isMeasurableFun.Build d _ aT rT f (set_mem (sub_sfun_mfun fP)).
+  @isMeasurableFun.Build d _ aT T f (set_mem (sub_sfun_mfun fP)).
 #[local] HB.instance Definition _ := sfun_Sub1_subproof.
 Definition sfun_Sub2_subproof :=
-  @FiniteImage.Build aT rT f (set_mem (sub_sfun_fimfun fP)).
+  @FiniteImage.Build aT T f (set_mem (sub_sfun_fimfun fP)).
 
 Import HBSimple.
 
@@ -106,8 +90,8 @@ Import HBSimple.
 Definition sfun_Sub := [sfun of f].
 End Sub.
 
-Lemma sfun_rect (K : T -> Type) :
-  (forall f (Pf : f \in sfun), K (sfun_Sub Pf)) -> forall u : T, K u.
+Lemma sfun_rect (K : Sf -> Type) :
+  (forall f (Pf : f \in sfun), K (sfun_Sub Pf)) -> forall u : Sf, K u.
 Proof.
 move=> Ksub [f [[Pf1] [Pf2]]]; have Pf : f \in sfun by apply/andP; rewrite ?inE.
 have -> : Pf1 = set_mem (sub_sfun_mfun Pf) by [].
@@ -120,17 +104,17 @@ Import HBSimple.
 Lemma sfun_valP f (Pf : f \in sfun) : sfun_Sub Pf = f :> (_ -> _).
 Proof. by []. Qed.
 
-HB.instance Definition _ := isSub.Build _ _ T sfun_rect sfun_valP.
+HB.instance Definition _ := isSub.Build _ _ Sf sfun_rect sfun_valP.
 
-Lemma sfuneqP (f g : {sfun aT >-> rT}) : f = g <-> f =1 g.
+Lemma sfuneqP (f g : {sfun aT >-> T}) : f = g <-> f =1 g.
 Proof. by split=> [->//|fg]; apply/val_inj/funext. Qed.
 
-HB.instance Definition _ := [Choice of {sfun aT >-> rT} by <:].
+HB.instance Definition _ := [Choice of {sfun aT >-> T} by <:].
 
 (* NB: already in cardinality.v *)
-HB.instance Definition _ x : @FImFun aT rT (cst x) := FImFun.on (cst x).
+HB.instance Definition _ x : @FImFun aT T (cst x) := FImFun.on (cst x).
 
-Definition cst_sfun x : {sfun aT >-> rT} := cst x.
+Definition cst_sfun x : {sfun aT >-> T} := cst x.
 
 Lemma cst_sfunE x : @cst_sfun x =1 cst x. Proof. by []. Qed.
 
@@ -148,3 +132,54 @@ Lemma fctZ (T : Type) (K : pzRingType) (L : lmodType K) k (f : T -> L) :
 Proof. by []. Qed.
 Arguments cst _ _ _ _ /.
 Definition fctWE := (fctD, fctN, fctM, fctZ).
+
+Section module.
+Context d (aT : measurableType d) (rT : realType) (nT : normedModType rT).
+
+Lemma sfun_subsemimod_closed : subsemimod_closed (@sfun d aT nT).
+Proof.
+split=> [|f g]. split=> [|f g]. all: rewrite ?inE/=.
+  apply/andP; split. rewrite /mfun in_setE/=.
+   move=> /andP[/= mf ff] /andP[/= mg fg]; rewrite !(rpredB, rpredM).
+Qed.
+
+HB.instance Definition _ := GRing.isSubringClosed.Build _ sfun
+  sfun_subring_closed.
+HB.instance Definition _ := [SubChoice_isSubComPzRing of {sfun aT >-> rT} by <:].
+
+Implicit Types (f g : {sfun aT >-> rT}).
+
+Import HBSimple.
+
+Lemma sfun0 : (0 : {sfun aT >-> rT}) =1 cst 0. Proof. by []. Qed.
+Lemma sfun1 : (1 : {sfun aT >-> rT}) =1 cst 1. Proof. by []. Qed.
+Lemma sfunN f : - f =1 \- f. Proof. by []. Qed.
+Lemma sfunD f g : f + g =1 f \+ g. Proof. by []. Qed.
+Lemma sfunB f g : f - g =1 f \- g. Proof. by []. Qed.
+Lemma sfunM f g : f * g =1 f \* g. Proof. by []. Qed.
+Lemma sfun_sum I r (P : {pred I}) (f : I -> {sfun aT >-> rT}) (x : aT) :
+  (\sum_(i <- r | P i) f i) x = \sum_(i <- r | P i) f i x.
+Proof. by elim/big_rec2: _ => //= i y ? Pi <-. Qed.
+Lemma sfun_prod I r (P : {pred I}) (f : I -> {sfun aT >-> rT}) (x : aT) :
+  (\sum_(i <- r | P i) f i) x = \sum_(i <- r | P i) f i x.
+Proof. by elim/big_rec2: _ => //= i y ? Pi <-. Qed.
+Lemma sfunX f n : f ^+ n =1 (fun x => f x ^+ n).
+Proof. by move=> x; elim: n => [|n IHn]//; rewrite !exprS sfunM/= IHn. Qed.
+
+HB.instance Definition _ f g := MeasurableFun.copy (f \+ g) (f + g).
+HB.instance Definition _ f g := MeasurableFun.copy (\- f) (- f).
+HB.instance Definition _ f g := MeasurableFun.copy (f \- g) (f - g).
+HB.instance Definition _ f g := MeasurableFun.copy (f \* g) (f * g).
+
+Import HBSimple.
+
+(* TODO: mv to `measurable_realfun.v`? *)
+HB.instance Definition _ (D : set aT) (mD : measurable D) :
+   @FImFun aT rT (mindic _ mD) := FImFun.on (mindic _ mD).
+Definition indic_sfun (D : set aT) (mD : measurable D) : {sfun aT >-> rT} :=
+  mindic rT mD.
+
+HB.instance Definition _ k f := MeasurableFun.copy (k \o* f) (f * cst_sfun k).
+Definition scale_sfun k f : {sfun aT >-> rT} := k \o* f.
+
+End ring.
--- theories/measure_theory/measurable_structure.v
+++ theories/measure_theory/measurable_structure.v
@@ -1318,32 +1318,30 @@ Let G := @open T.
 Definition measurableTop : set (set T) :=
   G.-sigma.-measurable.
 
-Definition measurable_of_topological : measurableType G .-sigma := g_sigma_algebraType G.
+Definition measurable_of_topological : measurableType G.-sigma := g_sigma_algebraType G.
 
-Let measurable0T : measurableTop set0. Proof. exact: sigma_algebra0. Qed.
-Let measurableCT : forall A, measurableTop A -> measurableTop (~` A). 
+Lemma measurable0T : measurableTop set0. Proof. exact: sigma_algebra0. Qed.
+Lemma measurableCT : forall A, measurableTop A -> measurableTop (~` A). 
 Proof. exact: sigma_algebraC. Qed.
-Let measurable_bigcupT : forall F : (set T)^nat, (forall i, measurableTop (F i)) -> 
+Lemma measurable_bigcupT : forall F : (set T)^nat, (forall i, measurableTop (F i)) -> 
 measurableTop (\bigcup_i (F i)). Proof. exact: sigma_algebra_bigcup. Qed.    
 
 Definition measurableTypeTop := g_sigma_algebraType G.
 
-HB.instance Definition Top_isMeasurable :
-  isMeasurable default_measure_display T :=
-  @isMeasurable.Build _ measurableTypeTop measurableTop
-    measurable0T measurableCT measurable_bigcupT.
-
-(* Not useful, only to check that it works*)
-Lemma open_in_sig (A : set T) : A \in @open T -> A \in <<s G>>.
-Proof. rewrite inE /smallest=> oA. rewrite inE => H /= [_ GH]. 
-by apply: GH. Qed.
-
 End Topological_sigmaring.
 
-Section Cast_Test.
-Variables (R : realType) (N : normedModType R).
-Check measurable_of_topological N : measurableType _.
-End Cast_Test.
+Module HBTopMeas.
+
+(* To stop making the non forgetful inheritance warning, topologicalType would have to be
+  below measurableTypes in the hierarchy *)
+HB.instance Definition _ (T : topologicalType) := Choice.on (@measurableTypeTop T).
+
+HB.instance Definition Top_isMeasurable (T : topologicalType) :
+  isMeasurable default_measure_display T := @isMeasurable.Build _
+  (measurableTypeTop T) (@measurableTop T) 
+  (@measurable0T T) (@measurableCT T) (@measurable_bigcupT T).
+
+End HBTopMeas.
 
 Section measurability.
 (* Bug here : it doesn't know what preimage_set_system is*)
