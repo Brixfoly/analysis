09b09ec37be92ac5fcc6f60a899509856e9d0b48
adjevahi <arthur.djevahirdjian@ens-lyon.fr>
2026-05-27 12:40:01 +0200

    less imports on meas_struct.v

--- theories/measure_theory/measurable_structure.v
+++ theories/measure_theory/measurable_structure.v
@@ -4,6 +4,10 @@ From mathcomp Require Import all_ssreflect_compat algebra finmap.
 #[warning="-warn-library-file-internal-analysis"]
 From mathcomp Require Import unstable.
 From mathcomp Require Import boolp classical_sets functions cardinality reals.
+<<<<<<< HEAD
+=======
+From mathcomp Require Import ereal sequences.
+>>>>>>> b7fff898a (less imports on meas_struct.v)
 
 (**md**************************************************************************)
 (* # Measure Theory                                                           *)
