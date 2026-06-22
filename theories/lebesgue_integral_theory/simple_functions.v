179b940ceebdab8986dba273a5ec2d8462809004
adjevahi <arthur.djevahirdjian@ens-lyon.fr>
2026-06-22 10:50:01 +0200

    started work on top of pr 1988

--- theories/lebesgue_integral_theory/simple_functions.v
+++ theories/lebesgue_integral_theory/simple_functions.v
@@ -251,6 +251,9 @@ Definition scale_sfun k f : {sfun aT >-> nT} := k *: f.
 Lemma sfun0r : (0 : {sfun aT >-> R}) =1 cst 0.
 Lemma sfun0m {V : normedModType R} : (0 : {sfun aT >-> V}) =1 cst 0.
 
+Lemma sfun0r : (0 : {sfun aT >-> R}) =1 cst 0.
+Lemma sfun0m {V : normedModType R} : (0 : {sfun aT >-> V}) =1 cst 0.
+
 End sfun_lmodType.
 
 Section ring.
